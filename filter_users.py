025e7b551b5b323185ce62212b326845610ca125
Yasin Akkus <akkusoft@gmail.com>
2026-06-03 00:53:39 +0200

    Ordered and structured the code, added a module docstring and corrected the entry-prompt to the user containing all new three features to filter by.

--- filter_users.py
+++ filter_users.py
@@ -1,49 +1,55 @@
+"""
+Filter users by age, name and email
+"""
 import json
 
 
 def filter_users_by_name(name):
-	"""Name filtering by that option"""
-	with open("users.json", "r") as file:
-		users = json.load(file)
-	
-	filtered_users = [user for user in users if user["name"].lower() == name.lower()]
-	
-	for user in filtered_users:
-		print(user)
+    """Name filtering by that option"""
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
+
+    for user in filtered_users:
+        print(user)
 
 
 def filter_by_age(age):
-	"""Age filtering by that option"""
-	with open("users.json", "r") as file:
-		users = json.load(file)
+    """Age filtering by that option"""
+    with open("users.json", "r") as file:
+        users = json.load(file)
 
-	filtered_users = [user for user in users if user["age"] == age]
+    filtered_users = [user for user in users if user["age"] == age]
 
-	for user in filtered_users:
-		print(user)
+    for user in filtered_users:
+        print(user)
 
 
 def filter_by_email(email):
-	with open("users.json", "r") as file:
-		users = json.load(file)
+    """Email filtering by that option"""
+    with open("users.json", "r") as file:
+        users = json.load(file)
 
-	filtered_users = [user for user in users if user["email"] == email]
+    filtered_users = [user for user in users if user["email"] == email]
 
-	for user in filtered_users:
-		print(user)
+    for user in filtered_users:
+        print(user)
 
 
 if __name__ == "__main__":
-	filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
-	
-	if filter_option == "name":
-		name_to_search = input("Enter a name to filter users: ").strip()
-		filter_users_by_name(name_to_search)
-	elif filter_option == "age":
-		age_to_search = int(input("Enter an age to filter users: ").strip())
-		filter_by_age(age_to_search)
-	elif filter_option == "email":
-		email_to_search = input("Enter an email to filter users: ").strip()
-		filter_by_email(email_to_search)
-	else:
-		print("Filtering by that option is not yet supported.")
+    filter_option = (input("""What would you like to filter by?
+(Currently 'name' 'age' and 'email' are supported): """)
+                     .strip().lower())
+
+    if filter_option == "name":
+        name_to_search = input("Enter a name to filter users: ").strip()
+        filter_users_by_name(name_to_search)
+    elif filter_option == "age":
+        age_to_search = int(input("Enter an age to filter users: ").strip())
+        filter_by_age(age_to_search)
+    elif filter_option == "email":
+        email_to_search = input("Enter an email to filter users: ").strip()
+        filter_by_email(email_to_search)
+    else:
+        print("Filtering by that option is not yet supported.")
